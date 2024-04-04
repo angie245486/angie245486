5982803c0a7dc7a99202e3af57aa983388565b01
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-04 09:23:20 -0500

    Create computadorpoo3.java

---
+++ computadorpoo3.java
@@ -0,0 +1,44 @@
+
+package CLASESYOBJETOS;
+
+public class computador 
+{
+    //atributos
+    String monitor;
+    String teclado;
+    String mouse;
+    String torre;
+    String marca;
+    String procesador;
+    String pulgadas;
+    String discoduro;
+    
+    
+    //metodo constructor
+    public computador(String monitor,String teclado, String mouse ,String torre)
+    {
+        this.monitor= marca;
+        this.monitor= pulgadas;
+        this.mouse= marca;
+        this.teclado= marca;
+        this.torre=discoduro;
+        this.torre=marca;
+        this.torre=procesador;
+        
+                
+        
+    }
+    
+    public void mostrardatos ()
+    {
+        System.out.println("la marca del monitor es: "+monitor+marca);
+        System.out.println("Las pulgadas del monitor: "+monitor+pulgadas);
+        System.out.println("la marca del teclado es: "+teclado+marca);
+        System.out.println("la marca del mouse es : "+mouse+marca);
+        System.out.println("la marca de la torre es: "+torre+marca);
+        System.out.println("el proc de la torre es: "+torre+procesador);
+        System.out.println("el disco duro de la torre es: "+torre+discoduro);
+        
+    }
+   
+}
