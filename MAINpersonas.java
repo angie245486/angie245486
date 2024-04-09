2ebd1315fb110c4e1daf0875ef8142d0029c1960
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-08 19:15:53 -0500

    Create MAINpersonas.java

---
+++ MAINpersonas.java
@@ -0,0 +1,15 @@
+
+package clasesyobjetoss;
+public class Main 
+{
+    public void main (String []args)
+    {
+        Personas p1 = new Personas ("Angie",19, "software","3125478654","carrera88c NO 63-67SUR");
+        
+        p1.mostrardatos();
+        
+    }
+    
+    
+    
+}
