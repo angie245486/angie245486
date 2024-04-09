23cbd4b1f6fe91f9b5670fc8f02a87fa2359fe23
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-08 19:35:32 -0500

    Create maincarropoo3.java

---
+++ maincarropoo3.java
@@ -0,0 +1,12 @@
+
+package ClasesyObjetosss;
+
+public class Main 
+{
+    public static void main(String []args)
+    {
+        carro c2=new carro("ford",10.000,1.0,"negro");
+        c2.mostrardatos();
+    }
+    
+}
