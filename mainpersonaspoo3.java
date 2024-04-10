8741ba165e84685976f8a3b3bfd6259090092d88
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-10 10:13:33 -0500

    Create mainpersonaspoo3.java

---
+++ mainpersonaspoo3.java
@@ -0,0 +1,22 @@
+
+package CLASESYOBJETOS;
+
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        Persona persona1 = new Persona("angie",19);
+        persona1.correr();
+    
+        Persona persona2 = new Persona("carrera88c n NO63-67sur");
+        persona2.correr( 100 );
+    
+        Persona persona3 = new Persona("la libertad");
+        persona3.correr(15);
+        
+        
+        
+    
+    }   
+}
+
