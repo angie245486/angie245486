7066ffd11a1f317289338a40181fd362d1aadd59
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-10 13:57:11 -0500

    Create nombre.cpp

---
+++ nombre.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nombres [2][10]=
+{
+		
+
+
+	{"Juan","Maria","Andres","Andi","Diego","Ana","Jose","Carlos","Juan","Diana"},
+	{"Daniel","Fernando","Santi","Duvan","Sebastian","Felipe","Angie","Mario","Orlando","Luz"}
+	
+};
+	cout<<"\nel nombre del estudiante 1 es:\n";
+	cout<< nombres [1][4];
+	cout<<"\nel nombre del estudiante 2 es:\n ";
+	cout<< nombres [1][9];
+	cout<<"\nel nombre del estudiante 3 es:\n";
+	cout<< nombres [0][4];
+	cout<<"\nel nombre del estudiante 4 es:\n ";
+	cout<< nombres [1][6];
+	cout<<"\nel nombre del estudiante 5 es:\n";
+	cout<< nombres [0][9];
+	
+	return 0;
+}
+	
+	
+
