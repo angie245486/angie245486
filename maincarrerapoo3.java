ac3f47ca9e70b6bcc231fa517659c4ff32352b3c
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-15 13:14:46 -0500

    Create maincarrerapoo3.java

---
+++ maincarrerapoo3.java
@@ -0,0 +1,12 @@
+
+package carrera;
+
+public class Main 
+{
+    public static void main (String []args)
+    {
+        CARRERA1 c1= new CARRERA1 ("angie","rincon",19,2,"software");
+        
+        c1.correr();
+    }
+}
