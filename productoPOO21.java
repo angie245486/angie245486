a562d467340ebcde577eae95a85e1a4da9a2f082
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-04-23 21:58:04 -0500

    Create productoPOO21.java

---
+++ productoPOO21.java
@@ -0,0 +1,37 @@
+
+package PRODUCTO;
+
+public class Producto 
+{
+    //atributos
+    private String codigo;
+    private String descripcion;
+    private float precio;
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+    
+    
+    
+}
